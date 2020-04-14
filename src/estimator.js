70b2579e4338f6b7a98f3470459b50a724c0ad9d
Nwafor-Onyebuchi <buchilazarus4@gmail.com>
2020-04-14 14:49:21 +0100

    Initial commit

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,10 @@
-const covid19ImpactEstimator = (data) => data;
 
-export default covid19ImpactEstimator;
+const { impactEstimator } = require('./helpers');
+
+const covid19ImpactEstimator = ((data) => {
+  const impact = impactEstimator({ ...data }, 10);
+  const severeImpact = impactEstimator({ ...data }, 50);
+  return { data, impact, severeImpact };
+});
+
+module.exports = covid19ImpactEstimator;
